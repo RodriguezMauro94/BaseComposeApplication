b44e2dc91015d8dcbe0a1431759953825432376f
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2024-12-05 09:22:42 -0300

    linked hilt

---
+++ app/src/main/java/com/maurosergiorodriguez/basecomposeapplication/BaseApplication.kt
@@ -0,0 +1,7 @@
+package com.maurosergiorodriguez.basecomposeapplication
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application()
